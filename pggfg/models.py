3fd7809fb36b9fc55769a92a9765455a7879dcbe
chapkovski <chapkovski@gmail.com>
2019-07-16 10:49:15 +0300

    switch to 2 rounds

--- pggfg/models.py
+++ pggfg/models.py
@@ -17,7 +17,7 @@ class Constants(BaseConstants):
     name_in_url = 'pggfg'
     players_per_group = 3
     num_others_per_group = players_per_group - 1
-    num_rounds = 1
+    num_rounds = 2
     rounds = list(range(1, num_rounds + 1))
     instructions_template = 'pggfg/includes/Instructions.html'
     endowment = 20
